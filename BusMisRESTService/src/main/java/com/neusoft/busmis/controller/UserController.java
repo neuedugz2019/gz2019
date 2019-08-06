0ca4657ab5a9ad12c34e997d8ade008aabe978f0
lhd <haidonglu@126.com>
2019-08-06 19:16:48 +0800

    0906-001

--- BusMisRESTService/src/main/java/com/neusoft/busmis/controller/UserController.java
+++ BusMisRESTService/src/main/java/com/neusoft/busmis/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.neusoft.busmis.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value="/user")
 public class UserController {
-	@RequestMapping(value="/info")
+	@RequestMapping(value="/info",method=RequestMethod.GET)
 	public String getInfo() throws Exception{
 		
 		return "OK";
